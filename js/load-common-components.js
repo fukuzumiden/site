84ba8ed6625286829691ee5a95d7262fa13d9b37
fukuzumiden <nepia.premiumsoft360@gmial.com>
2022-07-10 02:48:15 +0900

    絵のページ追加

---
+++ js/load-common-components.js
@@ -0,0 +1,23 @@
+function header(){
+    $.ajax({
+        url: "/common/header.html",
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            document.write(html);
+        }
+    });
+}
+
+function footer(){
+    $.ajax({
+        url: "/common/footer.html",
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            document.write(html);
+        }
+    });
+}
